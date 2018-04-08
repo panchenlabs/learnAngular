f07e718fcc5554344f866511d5cd287bf0f6f66c
Peter Chen <pchen1126@gmail.com>
2018-04-08 18:07:24 -0400

    restart udemy course

--- my-first-app/e2e/app.e2e-spec.ts
+++ my-first-app/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { MyFirstAppPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('my-first-app App', () => {
-  let page: MyFirstAppPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new MyFirstAppPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
